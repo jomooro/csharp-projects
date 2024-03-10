8751d0b4f2568996cfc02d7df538015594e697a3
John Moorosi <148855517+jomooro@users.noreply.github.com>
2024-03-11 01:34:13 +0200

    Add files via upload

---
+++ bubble-sort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class BubbleSort
+{
+    static void Main()
+    {
+        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+
+        Console.WriteLine("Original array: ");
+        PrintArray(array);
+
+        BubbleSortAlgorithm(array);
+
+        Console.WriteLine("\nSorted array: ");
+        PrintArray(array);
+    }
+
+    static void BubbleSortAlgorithm(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                // Compare adjacent elements
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap if they are in the wrong order
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var element in arr)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
