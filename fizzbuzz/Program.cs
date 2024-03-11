345c81d9441fa7ecbf177d926f29e974b29563da
John Moorosi <148855517+jomooro@users.noreply.github.com>
2024-03-11 02:42:32 +0200

    Add files via upload

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public string CheckNumber(int number)
+    {
+        if (number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
+        if (number % 3 == 0)
+            return "Fizz";
+        if (number % 5 == 0)
+            return "Buzz";
+        
+        return number.ToString();
+    }
+
+    public string CountTo(int limit)
+    {
+        List<string> result = new List<string>();
+
+        for (int i = 1; i <= limit; i++)
+        {
+            result.Add(CheckNumber(i));
+        }
+
+        return "[" + string.Join(", ", result) + "]";
+    }
+
+    static void Main()
+    {
+        Program program = new Program();
+        Console.WriteLine(program.CountTo(15));
+    }
+}
